e5142b541494d6370ae096e4474f07df4c2a45f9
tiankchen <tiankongcc1991@qq.com>
2017-11-28 16:51:00 +0800

    update project structure

---
+++ java/mybatistest/src/main/java/Main.java
@@ -0,0 +1,21 @@
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Main {
+
+    public static  void main(String args[]){
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = null;
+        try {
+            inputStream = Resources.getResourceAsStream(resource);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        SqlSessionFactory sqlSessionFactory =
+                new SqlSessionFactoryBuilder().build(inputStream);
+    }
+}

7d8fd490d918f77ffa376470129056cf661f94d4
tiankchen <tiankongcc1991@qq.com>
2017-12-03 23:21:07 +0800

    update mybatis test

---
+++ java/mybatistest/src/main/java/com/chenchen/Main.java
@@ -0,0 +1,55 @@
+package com.chenchen;
+
+import com.chenchen.dao.UserMapper;
+import com.chenchen.models.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.Reader;
+
+public class Main {
+
+    private static SqlSessionFactory sqlSessionFactory;
+    private static Reader reader;
+
+    static {
+        try {
+            reader = Resources.getResourceAsReader("mybatis-config.xml");
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            sqlSessionFactory.getConfiguration().addMapper(UserMapper.class);
+        } catch (Exception e) {
+            System.out.println("Catch exception");
+            e.printStackTrace();
+        }
+    }
+
+    public static SqlSessionFactory getSession() {
+        return sqlSessionFactory;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            UserMapper userMapper = session.getMapper(UserMapper.class);
+            User user = userMapper.getUserByID(1);
+            if (user != null) {
+                String userInfo = "名字：" + user.getName() + ", 所属部门：" + user.getDept() + ", 主页：" + user.getWebsite();
+                System.out.println(userInfo);
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("Catch exception");
+            e.printStackTrace();
+        }
+        finally {
+            session.close();
+        }
+    }
+}
